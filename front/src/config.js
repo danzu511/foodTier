19a61fea418fc7e079ed104a3f203762a0ccf2fe
danzu511 <daneriklundstrom@gmail.com>
2023-10-31 17:03:57 +0200

    Last dev commit

--- front/src/config.js
+++ front/src/config.js
@@ -1,2 +1,1 @@
-//export const MY_API = process.env.MY_API || "http://localhost:3001"
-export const MY_API = 'https://foodtier.onrender.com'
+export const MY_API = process.env.MY_API || "http://localhost:3001"
